b0f2a31d0d9c3f2fe05bfae0388d19830fd17d73
Jessie Smith <jessie.smit01@gmail.com>
2020-02-17 12:22:54 -0600

    added files to repo

---
+++ src/components/CardSection.vue
@@ -0,0 +1,25 @@
+<template>
+    <b-container>
+        <template v-if="option === 'projects'">
+            <b-card-group deck>
+                <b-card
+                    title="Workout Tracker">
+                    <b-card-text>
+                        screenshot coming soon.
+                    </b-card-text>
+                    
+                </b-card>
+            </b-card-group>
+        </template>
+        
+    </b-container>
+</template>
+<script>
+export default {
+    name: "CardSection",
+    props:{
+        option: String
+    }
+    
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,63 @@
+<template>
+ <b-container>
+    <b-jumbotron
+    fluid
+    header="Jessie Smith"
+    lead="Junior Web Developer"
+    variant="outline-secondary"
+    >
+        <b-container fluid>
+            <b-row>
+                <b-col>
+                    <b-button class="mr-1" href="https://github.com/JessieSmit01" variant="outline-dark"><i class="fab fa-github"></i> Github</b-button>
+                    <b-button class="mr-1" href="https://www.linkedin.com/in/jessie-smith-3573a91a0/" variant="outline-primary"><i class="fab fa-linkedin"></i> LinkedIn</b-button>
+
+                </b-col>
+            </b-row>
+         </b-container>
+       
+    </b-jumbotron>
+     <b-container>
+            <b-button-group>
+                <b-button variant="outline-dark" @click="handleProjOpt">Projects</b-button>
+             </b-button-group>
+
+            <card-section :option="option"></card-section>
+        </b-container>
+
+        
+        
+
+ </b-container>
+ 
+    
+</template>
+
+<script>
+import CardSection from './CardSection.vue'
+
+export default {
+    name: "Home",
+
+    data: function()
+    {
+        return{
+            option: 'projects'
+        }
+    },
+
+    components: {
+        CardSection
+    },
+    methods: {
+        handleProjOpt: function(){
+            this.option = "projects"
+        }
+    }
+
+}
+</script>
+
+<style>
+   
+</style>
